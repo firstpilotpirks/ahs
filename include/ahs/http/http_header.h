6718adf2f42ce64cb2ed5e835fdde6899b0ca745
pirks <firstpilotpirks@gmail.com>
2018-05-04 19:14:54 +0300

    travis test

--- include/ahs/http/http_header.h
+++ include/ahs/http/http_header.h
@@ -8,15 +8,17 @@ class http_header
 public:
     using header_data = std::map<std::string, std::string>;
 
-    http_header(header_data & data);
-    http_header(header_data && data);
+    http_header(const http_header & header) = default;
+    http_header(http_header && header) = default;
 
-    std::string & operator[](const std::string &field);
-    std::string & operator[](std::string &&field);
+    explicit http_header(const header_data & data);
+    explicit http_header(header_data && data);
+
+    std::string & operator[](const std::string & field);
+    std::string & operator[](std::string && field);
 
 private:
     header_data _data;
 };
-
 
 #endif // HTTP_HEADER_H
--- src/ahs/http/http_header.cpp
+++ src/ahs/http/http_header.cpp
@@ -1,6 +1,6 @@
 #include "ahs/http/http_header.h"
 
-http_header::http_header(http_header::header_data &data) :
+http_header::http_header(const http_header::header_data &data) :
 _data(data)
 {
 
